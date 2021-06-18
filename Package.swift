3cad080324d201422850eeb4e041082f030c82f2
Ярослав Стрельников <mr.strelnikov9@gmail.com>
2021-06-18 13:28:28 +0500

    Create first release

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let baseURL: String = "https://https://github.com/wingssoft/WingsPushSdkiOS_SPM/releases/download/"
-let versionPackage: String = "2.4.32"
+let versionPackage: String = "2.4.31"
 let packageName = "WingsPushSDK.xcframework.zip"
 let checksum: String = "52f8aac4b26c37395371abaf9487171c1fd06a6d64341a1845d0a54598a20aae"
 
 let package = Package(
-    name: "WingsPushSdkiOS_SPM",
+    name: "WingsPushSDK",
     platforms: [
         .iOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "WingsPushSdkiOS_SPM",
-            targets: ["WingsPushSdkiOS_SPM"]),
+            name: "WingsPushSDK",
+            targets: ["WingsPushSDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -25,6 +25,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "WingsPushSdkiOS_SPM", url: "\(baseURL)\(versionPackage)/\(packageName)", checksum: checksum),
+        .binaryTarget(name: "WingsPushSDK", url: "\(baseURL)\(versionPackage)/\(packageName)", checksum: checksum),
     ]
 )
