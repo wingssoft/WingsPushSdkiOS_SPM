15a61d4f1b6b4c7e0e78b593787a44862d368331
Ярослав Стрельников <mr.strelnikov9@gmail.com>
2021-07-08 15:22:10 +0500

    Fix 404 Error

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "WingsPushSDK",
+    name: "WingsPushSDKPackage",
     platforms: [
         .iOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "WingsPushSDK",
+            name: "WingsPushSDKPackage",
             targets: ["WingsPushSDK"]),
     ],
     dependencies: [
