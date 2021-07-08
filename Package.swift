7b843faf2b7b7e05930bc64a8b96ee01c8a959b7
Ярослав Стрельников <mr.strelnikov9@gmail.com>
2021-07-08 15:17:20 +0500

    Create locally binary artifact

--- Package.swift
+++ Package.swift
@@ -3,11 +3,6 @@
 
 import PackageDescription
 
-let baseURL: String = "https://github.com/wingssoft/WingsPushSdkiOS_SPM/releases/download/"
-let versionPackage: String = "2.4.32"
-let packageName = "WingsPushSDK.xcframework.zip"
-let checksum: String = "99212d804d4351b38a9d56f5343b6f42dc10c2d155c5643b65b5b59619360c28"
-
 let package = Package(
     name: "WingsPushSDK",
     platforms: [
@@ -25,6 +20,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "WingsPushSDK", url: "\(baseURL)\(versionPackage)/\(packageName)", checksum: checksum),
+        .binaryTarget(name: "WingsPushSDK", path: "./artifacts/WingsPushSDK.xcframework")
     ]
 )
---
+++ artifacts/WingsPushSDK.xcframework/ios-arm64_x86_64-simulator/WingsPushSDK.framework/Headers/WingsPushSDK.h
@@ -0,0 +1,19 @@
+//
+//  WingsPushSDK.h
+//  WingsPushSDK
+//
+//  Created by Balashov Ilya on 04.12.2019.
+//  Copyright © 2019 Balashov Ilya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WingsPushSDK.
+FOUNDATION_EXPORT double WingsPushSDKVersionNumber;
+
+//! Project version string for WingsPushSDK.
+FOUNDATION_EXPORT const unsigned char WingsPushSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WingsPushSDK/PublicHeader.h>
+
+
