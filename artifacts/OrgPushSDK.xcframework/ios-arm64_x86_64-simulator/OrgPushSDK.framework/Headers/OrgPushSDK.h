883213d71c3e66e0031e4b9827f4d9e7c66c1cb7
nullptr <mr.strelnikov9@gmail.com>
2023-12-20 12:38:58 +0500

    Update SDK

---
+++ artifacts/OrgPushSDK.xcframework/ios-arm64_x86_64-simulator/OrgPushSDK.framework/Headers/OrgPushSDK.h
@@ -0,0 +1,21 @@
+//
+//  PushSDK.h
+//  PushSDK
+//
+//  Created by Balashov Ilya on 04.12.2019.
+//  Copyright © 2019 Balashov Ilya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "sqlite3.h"
+#import "shim.h"
+#import "grdb_config.h"
+#import "GRDB.h"
+
+//! Project version number for PushSDK.
+FOUNDATION_EXPORT double PushSDKVersionNumber;
+
+//! Project version string for PushSDK.
+FOUNDATION_EXPORT const unsigned char PushSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PushSDK/PublicHeader.h>
